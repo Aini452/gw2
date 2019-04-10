e5bfa4a806517ab3affc9eb8189e840baa64089e
Acapulcohood <adelabre@humanbooster.com>
2019-04-10 10:11:53 +0200

    ajout http

--- src/app/services/wallet.service.ts
+++ src/app/services/wallet.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class WalletService {
+  API_URL = 'https://api.guildwars2.com/v2/account/wallet';
+  API_KEY = '95D96FCA-7E23-834E-A4D4-0E2CCF0603C5DE4D9A3B-2686-4DBF-B1FE-3BAE731EC048';
+
  // https://wiki.guildwars2.com/wiki/API:2/account/wallet à mettre en lien avec https://wiki.guildwars2.com/wiki/API:2/currencies
   constructor() { }
 }
