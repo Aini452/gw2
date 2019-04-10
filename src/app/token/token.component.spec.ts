aba9497b7f2e111164f9de34a96c8f1f17672575
Acapulcohood <adelabre@humanbooster.com>
2019-04-10 16:34:19 +0200

    ajout test  token

--- src/app/token/token.component.spec.ts
+++ src/app/token/token.component.spec.ts
@@ -1,25 +1,52 @@
+import { WalletService } from '../services/wallet.service';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TokenComponent } from './token.component';
+import { of } from 'rxjs';
+import { Token } from '../model/token.model';
 
-describe('TokenComponent', () => {
+
+fdescribe('TokenComponent', () => {
   let component: TokenComponent;
   let fixture: ComponentFixture<TokenComponent>;
+  const fakeWalletService = jasmine.createSpyObj('WalletService',
+    {
+      getToken: of({
+        name: 'NameTest',
+        description: 'DescriptionTest',
+        icon: 'IconTest'
+      })
+    }
+  );
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TokenComponent ]
+      declarations: [TokenComponent],
+      providers: [
+        { provide: WalletService, useValue: fakeWalletService }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TokenComponent);
     component = fixture.componentInstance;
+    component.wallet = { id: 0, value: 10}as Token;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create the right method', () => {
+    expect(component.ws.getToken).toHaveBeenCalled();
+  });
+
+  it('should show the right poster', () => {
+    const img = fixture.nativeElement.querySelector('img');
+    expect(img.src).toContain('IconTest',
+      'The icon test must show');
+  });
+
 });
